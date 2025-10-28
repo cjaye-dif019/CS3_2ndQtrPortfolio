7ac8f4da2bd68ec35aef192df0b65d70582df820
cjaye-dif019 <b2029cjadifuntorum@pshs.edu.ph>
2025-10-28 20:40:15 +0800

    part 8

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
+const menuToggle = document.getElementById('toggle');
+const menu = document.getElementById('navbar2');
 
 menuToggle.addEventListener('click', () => 
 {
@@ -9,3 +9,21 @@ menuToggle.addEventListener('click', () =>
     icon.classList.toggle('fa-bars');
      icon.classList.toggle('fa-xmark');
 });
+
+    const audio = document.getElementById("sound");
+    const button = document.getElementById("icon1");
+    const icon = button.querySelector('i');
+
+    button.addEventListener("click", function () {
+        if (audio.paused) {
+            audio.play();
+            icon.classList.remove("fa-play");  
+            icon.classList.add("fa-pause");   
+        } 
+        
+        else {
+            audio.pause();
+            icon.classList.remove("fa-pause"); 
+            icon.classList.add("fa-play");     
+        }
+    });
